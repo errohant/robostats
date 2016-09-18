11793cd380f33642cabcb86ca9cd570b49e38850
path <whizkidrohan@gmail.com>
2016-09-18 16:37:30 -0400

    Added plots

--- hw1/sol.py
+++ hw1/sol.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import random
 
 class Nature:
@@ -6,7 +7,7 @@ class Nature:
         return -1 if random.random() <= 0.5 else 1
     def get_determ_y(self,t):
         """Send deterministic y"""
-        return -1 if t%2==1 else 1
+        return -1 if t%3==1 else 1
     def get_adver_y(self,t):
         """Send aversarial output"""
         return -1 if t%2==0 else 1
@@ -34,17 +35,21 @@ class Algorithm:
         self.nature = Nature()
 
     def wma(self):
-        self.regret = [0,0,0]
+        self.regret = [0.0,0.0,0.0]
         w = [1.0,1.0,1.0]
         eta = 0.4
-        for t in range(10):
+        learner_loss = 0
+        for t in range(1000):
             xt = self.expert.get_x(t)
             yt_hat = -1 if sum(i[0]*i[1] for i in zip(w,xt)) <= 0 else 1
-            yt = self.nature.get_determ_y(t)
+            yt = self.nature.get_stoch_y()
             for i in range(len(xt)):
+                learner_loss = learner_loss + (0 if yt == yt_hat else 1)
                 w[i] = w[i]*(1 - eta * (0 if yt == xt[i] else 1))
                 self.regret[i] = self.regret[i] + (0 if yt == yt_hat else 1) - (0 if yt == xt[i] else 1)
-            print "xt=" + str(xt) + " yt=" + str(yt) +  " yt_hat=" + str(yt_hat) + " w=" + str(w)
+            print "xt=" + str(xt) + " yt=" + str(yt) +  " yt_hat=" + str(yt_hat) + " w=" + str(w) + " r=" + str(self.regret)
+            plt.plot(t, (self.regret[0]+self.regret[1]+self.regret[2])/3, 'ro')
+        plt.show()
 
 def main():
     algorithm = Algorithm()
